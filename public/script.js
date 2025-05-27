7e17012e287efa62c473a9d3b79d35a0514f68fd
Lucas Siqueira Campo Dall Orto <lucascampo.siqueira@gmail.com>
2025-05-27 06:44:57 -0300

    Início JavaScript

---
+++ public/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    // Elementos do DOM
+    const inputTarefa = document.getElementById('campo');
+    const btnCriar = document.getElementById('btn-campo');
+    const faixaTarefas = document.querySelector('.faixa');
+});
